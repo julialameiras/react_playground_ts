6d6126909b9156e2bc63057cb4fb5a5dc8c7564a
julia <julialameiras@gmail.com>
2024-08-22 11:46:31 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,27 @@
+import './App.css'
+import Contador from './pages/contador/Contador'
+import Home from './pages/home/Home'
+import Tarefa from './pages/tarefa/Tarefa'
+
+function App() {
+  //const [nome_estado, função_setState] = useState(valor_inicial_do_estado)
+
+  return (
+    <>
+      {/*<Home
+        titulo = "Minha primeira postagem"
+        texto = "Texto da minha primeira postagem" 
+      />
+      
+      <Home
+        titulo = "Minha primeira postagem"
+        texto = "Texto da minha primeira postagem" 
+      />*/}
+
+      {/*<Contador/>*/}
+      <Tarefa/>
+    </>
+  )
+}
+
+export default App
